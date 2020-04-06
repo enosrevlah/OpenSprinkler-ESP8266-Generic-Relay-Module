28d24824ea5c56256662c04412ab09c695d8d5f9
enosrevlah <51892324+enosrevlah@users.noreply.github.com>
2020-04-05 22:24:14 -0700

    Update defines.cpp

--- defines.cpp
+++ defines.cpp
@@ -4,12 +4,15 @@ byte PIN_BUTTON_1 = 255;
 byte PIN_BUTTON_2 = 255;
 byte PIN_BUTTON_3 = 255;
 byte PIN_BUTTON_4 = 255;
-byte PIN_RELAY_1 = 5; //D1
-byte PIN_RELAY_2 = 4; //D2
-byte PIN_RELAY_3 = 0; //D3
-byte PIN_RELAY_4 = 14; //D5
-byte PIN_RELAY_5 = 12; //D6
-byte PIN_LED = 2; //D4
+byte PIN_RELAY_1 = 12; 
+byte PIN_RELAY_2 = 13; 
+byte PIN_RELAY_3 = 14; 
+byte PIN_RELAY_4 = 16; 
+byte PIN_RELAY_5 = 4; 
+byte PIN_RELAY_6 = 5; 
+byte PIN_RELAY_7 = 15; 
+byte PIN_RELAY_8 = 10; 
+byte PIN_LED = 2; 
 byte PIN_RFRX = 255;
 byte PIN_RFTX = 255;
 byte PIN_BOOST = 255;
@@ -18,23 +21,24 @@ byte PIN_LATCH_COM = 255;
 byte PIN_SENSOR1 = 255;
 byte PIN_SENSOR2 = 255;
 byte PIN_RAINSENSOR = 255;
-byte PIN_FLOWSENSOR = 16; //D0
+byte PIN_FLOWSENSOR = 255; //D0
 
 /*
- GPIO_KEY1,        // GPIO00 Button 1
+ GPIO_KEY1,            // GPIO00 Board Jumper
      GPIO_USER,        // GPIO01 Serial RXD and Optional sensor
      GPIO_USER,        // GPIO02 Optional sensor
      GPIO_USER,        // GPIO03 Serial TXD and Optional sensor
-     GPIO_REL3,        // GPIO04 Sonoff 4CH Red Led and Relay 3 (0 = Off, 1 = On)
-     GPIO_REL2,        // GPIO05 Sonoff 4CH Red Led and Relay 2 (0 = Off, 1 = On)
+     GPIO_REL3,        // GPIO04 Green Led and Relay 1 (0 = Off, 1 = On)
+     GPIO_REL2,        // GPIO05 Green Led and Relay 2 (0 = Off, 1 = On)
                        // GPIO06 (SD_CLK   Flash)
                        // GPIO07 (SD_DATA0 Flash QIO/DIO/DOUT)
                        // GPIO08 (SD_DATA1 Flash QIO/DIO/DOUT)
-     GPIO_KEY2,        // GPIO09 Button 2
-     GPIO_KEY3,        // GPIO10 Button 3
+     GPIO_KEY2,        // GPIO09 
+     GPIO_KEY3,        // GPIO10 Green Led and Relay 3 (0 = Off, 1 = On)
                        // GPIO11 (SD_CMD   Flash)
-     GPIO_REL1,        // GPIO12 Red Led and Relay 1 (0 = Off, 1 = On)
-     GPIO_LED1_INV,    // GPIO13 Blue Led (0 = On, 1 = Off)
-     GPIO_KEY4,        // GPIO14 Button 4
-     GPIO_REL4,        // GPIO15 Red Led and Relay 4 (0 = Off, 1 = On)
+     GPIO_REL1,        // GPIO12 Green Led and Relay 4 (0 = Off, 1 = On)
+     GPIO_LED1_INV,    // GPIO13 Green Led and Relay 5 (0 = Off, 1 = On)
+     GPIO_KEY4,        // GPIO14 Green Led and Relay 6 (0 = Off, 1 = On)
+     GPIO_REL4,        // GPIO15 Green Led and Relay 7 (0 = Off, 1 = On)
+     GPIO_REL4,        // GPIO16 Green Led and Relay 8 (0 = Off, 1 = On)
 */
